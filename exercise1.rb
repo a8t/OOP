5528f5157ab6785304faa5efd9683d7619f96c17
Andy Tran <andy8tran@gmail.com>
2017-08-14 13:20:33 -0400

    finished player section

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,5 @@
+# Exercise 1
+
 class BankAccount
   attr_reader :balance, :interest_rate
 
@@ -24,6 +26,15 @@ class BankAccount
   end
 end
 
+andy = BankAccount.new(20000,1.005)
+# puts andy.deposit(1000)
+# puts andy.gain_interest
+# puts andy.withdraw(200)
+# puts andy.withdraw(40000)
+
+
+# Exercise 2
+
 class Cat
   attr_reader :name, :preferred_food, :meal_time
 
@@ -43,5 +54,79 @@ class Cat
 end
 
 lois = Cat.new("Lois","tuna",23)
-puts lois.eats_at
-puts lois.meow
+nala = Cat.new("Nala","treats",7)
+# puts lois.eats_at
+# puts nala.meow
+
+
+# Exercise 3
+
+class Player
+  attr_reader :gold_coins, :health_points, :lives
+
+  def initialize
+    @lives = 5
+    @gold_coins = 0
+    @health_points = 10
+  end
+
+
+
+  def level_up
+    @lives += 1
+  end
+
+  def collect_treasure
+    if @gold_coins % 10 == 0
+      level_up
+      @gold_coins += 1
+    else
+      @gold_coins += 1
+    end
+  end
+
+  def do_battle(damage)
+    if damage >= @health_points  && @lives > 1
+      @lives -= 1
+      @health_points = 10
+    elsif damage >= @health_points
+      restart
+    else
+      @health_points -= damage
+    end
+  end
+
+  def restart
+    initialize
+  end
+end
+
+john = Player.new
+#
+# puts john.lives
+# john.level_up
+# puts john.lives
+# puts john.collect_treasure
+# puts john.lives
+
+# john.do_battle(1)
+# puts "Lives: #{john.lives}, Health: #{john.health_points}"
+# john.do_battle(2)
+# puts "Lives: #{john.lives}, Health: #{john.health_points}"
+# john.do_battle(3)
+# puts "Lives: #{john.lives}, Health: #{john.health_points}"
+# john.do_battle(2)
+# puts "Lives: #{john.lives}, Health: #{john.health_points}"
+# john.do_battle(10)
+# puts "Lives: #{john.lives}, Health: #{john.health_points}"
+# john.do_battle(10)
+# puts "Lives: #{john.lives}, Health: #{john.health_points}"
+# john.do_battle(10)
+# puts "Lives: #{john.lives}, Health: #{john.health_points}"
+# john.do_battle(10)
+# puts "Lives: #{john.lives}, Health: #{john.health_points}"
+# john.do_battle(10)
+# puts "Lives: #{john.lives}, Health: #{john.health_points}"
+
+
+# Exercise 4
